09ef114fb1a3184a461ac1e9c9c495dbc6493f2f
c0a22049 <c0a220493f@edu.teu.ac.jp>
2023-11-28 14:56:17 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,21 @@ pg.K_LEFT: (-5, 0),
 pg.K_RIGHT: (+5, 0)
 }
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外を判定そ、真理値タプルを返す関数
+    引数rct：こうかとんor爆弾surfaceのrect
+    戻り値：横方向：縦方向はみ出し判定結果（画面内：True/画面外：False）
+    """
+    
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -20,8 +35,9 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rct = kk_img.get_rect()
-    kk_rct.centerx = 900, 400
+    kk_rct.center = 900, 400
     bb_img = pg.Surface((20, 20))  # 練習1：透明なsurfaceを作る
+    bb_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_rct = bb_img.get_rect()  # 練習2：surfaceのrectを抽出する
     bb_rct.centerx = random.randint(0, WIDTH)
@@ -35,21 +51,33 @@ def main():
             if event.type == pg.QUIT: 
                 return
             
+        if kk_rct.colliderect(bb_rct):
+            print("Game Over")
+            return
+            
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        for k , tpl in delta.item():
+        for k , tpl in delta.items():
             if key_lst[k]:
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
 
         screen.blit(bg_img, [0, 0])
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_img.move_ip(vx, vy)
+        bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+        bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
